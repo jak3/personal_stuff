edef0e885ec40f9d17268ac6fea4f3b4d185c642
jak3 <jak3lsa@gmail.com>
2014-01-17 10:51:22 +0100

    modified layout_script

--- scripts/layout_switch.sh
+++ scripts/layout_switch.sh
@@ -7,10 +7,10 @@
 if [[ -n "$1" ]]; then
     setxkbmap $1
 else
-    layout=$(setxkbmap -query | awk 'END{print $2}')
+    layout=$(setxkbmap -query | grep layout | awk '{print $2}')
     case $layout in
         us)
-                setxkbmap -layout it -model pc104
+                setxkbmap -model pc104 -layout it 
             ;;
         *)
                 setxkbmap us
