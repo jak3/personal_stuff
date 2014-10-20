5113e23ff7cfb983505f39b36ab575a1ba133a52
jack <jack@debian>
2014-10-20 16:31:39 +0200

    ignore all bundle dir plus a rocky integration of git-prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,64 +1,11 @@
-# Git-status from oh my zsh
-# ZSH Git Prompt Plugin from:
 # http://github.com/olivierverdier/zsh-git-prompt
-
-export __GIT_PROMPT_DIR=$HOME/.git-prompt
-# Initialize colors.
-autoload -U colors
-colors
-
-# Allow for functions in the prompt.
-setopt PROMPT_SUBST
-
-## Enable auto-execution of functions.
-typeset -ga preexec_functions
-typeset -ga precmd_functions
-typeset -ga chpwd_functions
-
-# Append git functions needed for prompt.
-preexec_functions+='preexec_update_git_vars'
-precmd_functions+='precmd_update_git_vars'
-chpwd_functions+='chpwd_update_git_vars'
-
-## Function definitions
-function preexec_update_git_vars() {
-    case "$2" in
-        git*)
-        __EXECUTED_GIT_COMMAND=1
-        ;;
-    esac
-}
-
-function precmd_update_git_vars() {
-    if [ -n "$__EXECUTED_GIT_COMMAND" ]; then
-update_current_git_vars
-        unset __EXECUTED_GIT_COMMAND
-    fi
-}
-
-function chpwd_update_git_vars() {
-    update_current_git_vars
-}
-
-function update_current_git_vars() {
-    unset __CURRENT_GIT_STATUS
-
-    local gitstatus="$__GIT_PROMPT_DIR/gitstatus.py"
-    _GIT_STATUS=`python ${gitstatus}`
-    __CURRENT_GIT_STATUS=("${(f)_GIT_STATUS}")
-}
-
-function prompt_git_info() {
-    if [ -n "$__CURRENT_GIT_STATUS" ]; then
-echo "(%{${fg[red]}%}$__CURRENT_GIT_STATUS[1]%{${fg[default]}%}$__CURRENT_GIT_STATUS[2]%{${fg[magenta]}%}$__CURRENT_GIT_STATUS[3]%{${fg[default]}%})"
-    fi
-}
+source $STUFF/zsh/zsh-git-prompt/zshrc.sh
 
 #Imported from oh-my-zsh project
 #--- https://raw.github.com/robbyrussell/oh-my-zsh/master/lib/spectrum.zsh
 # A script to make using 256 colors in zsh less painful.
 # P.C. Shyamshankar <sykora@lucentbeing.com>
-# Copied from http://github.com/sykora/etc/blob/master/zsh/functions/spectrum/
+# Source http://github.com/sykora/etc/blob/master/zsh/functions/spectrum/
 
 typeset -Ag FX FG BG
 
@@ -92,6 +39,6 @@ function spectrum_bls() {
 }
 
 PROMPT='
-$(prompt_git_info)%f %{$reset_color%}%{$FG[220]%}%~ %{$reset_color%}%{$FG[124]%}
+$(git_super_status)%f %{$reset_color%}%{$FG[220]%}%~ %{$reset_color%}%{$FG[124]%}
  %{$reset_color%}'
 RPROMPT='%{$FG[172]%}[%{$reset_color%}%(?,%{$FG[220]%}%?%{$reset_color%},%{$FG[124]%}%?%{$reset_color%})%{$FG[172]%}] %{$reset_color%}%{$FG[226]%}%n%{$reset_color%}'
